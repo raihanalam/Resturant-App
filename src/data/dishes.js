057635ac4e9cb5f9bdc6e13cfd451f65447c31e3
raihanalam <raihanhridoybd@gmail.com>
2023-05-04 03:23:19 +0600

    Menu Components Ready

---
+++ src/data/dishes.js
@@ -0,0 +1,134 @@
+const DISHES = [
+     {
+          id: 0,
+          name: 'Ginger Burger',
+          image: 'assets/images/burger.jpg',
+          category: 'meal',
+          label: 'Hot',
+          price: '299',
+          description: 'You don’t need to install or configure tools like webpack or Babel. They are preconfigured and hidden so that you can focus on the code. Create a project, and you’re good to go.',
+          comments: [
+               {
+                    id: 0,
+                    rating: 5,
+                    comments: 'Yummy and hot, living in a chezzz side.',
+                    author: "Shanto",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+               {
+                    id: 1,
+                    rating: 5,
+                    comments: 'Tasty and hot, living in a chezzz side.',
+                    author: "Shafin",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+               {
+                    id: 2,
+                    rating: 5,
+                    comments: 'Yummy and hot, living in a chezzz side.',
+                    author: "Kashif",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+          ]
+     },
+     {
+          id: 1,
+          name: 'Pasta  Burger',
+          image: 'assets/images/burger1.jpg',
+          category: 'meal',
+          label: 'Cool',
+          price: '355',
+          description: 'You don’t need to install or configure tools like webpack or Babel. They are preconfigured and hidden so that you can focus on the code. Create a project, and you’re good to go.',
+          comments: [
+               {
+                    id: 0,
+                    rating: 5,
+                    comments: 'Yummy and hot, living in a chezzz side.',
+                    author: "Shanto",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+               {
+                    id: 1,
+                    rating: 5,
+                    comments: 'Tasty and hot, living in a chezzz side.',
+                    author: "Shafin",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+               {
+                    id: 2,
+                    rating: 5,
+                    comments: 'Yummy and hot, living in a chezzz side.',
+                    author: "Kashif",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+          ]
+     },
+     {
+          id: 2,
+          name: 'Mini Stake Burger',
+          image: 'assets/images/burger2.jpg',
+          category: 'meal',
+          label: 'Hot',
+          price: '199',
+          description: 'You don’t need to install or configure tools like webpack or Babel. They are preconfigured and hidden so that you can focus on the code. Create a project, and you’re good to go.',
+          comments: [
+               {
+                    id: 0,
+                    rating: 5,
+                    comments: 'Yummy and hot, living in a chezzz side.',
+                    author: "Shanto",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+               {
+                    id: 2,
+                    rating: 5,
+                    comments: 'Tasty and hot, living in a chezzz side.',
+                    author: "Shafin",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+               {
+                    id: 3,
+                    rating: 5,
+                    comments: 'Yummy and hot, living in a chezzz side.',
+                    author: "Kashif",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+          ]
+     },
+     {
+          id: 3,
+          name: 'Naga BBQ Burger',
+          image: 'assets/images/burger3.jpg',
+          category: 'meal',
+          label: 'Hot',
+          price: '400',
+          description: 'You don’t need to install or configure tools like webpack or Babel. They are preconfigured and hidden so that you can focus on the code. Create a project, and you’re good to go.',
+          comments: [
+               {
+                    id: 0,
+                    rating: 5,
+                    comments: 'Yummy and hot, living in a chezzz side.',
+                    author: "Shanto",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+               {
+                    id: 2,
+                    rating: 5,
+                    comments: 'Tasty and hot, living in a chezzz side.',
+                    author: "Shafin",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+               {
+                    id: 3,
+                    rating: 5,
+                    comments: 'Yummy and hot, living in a chezzz side.',
+                    author: "Kashif",
+                    date: '2018-10-16T17:57:28.34534'
+               },
+          ]
+     }
+
+
+]
+
+export default DISHES 
