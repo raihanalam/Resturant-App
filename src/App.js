1fe347a74d35a90b477e0dc11926cf1ac7cab7bc
raihanalam <raihanhridoybd@gmail.com>
2023-05-04 00:27:38 +0600

    Components Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import MainComponent from './components/MainComponent'
 function App() {
   return (
     <div className="App">
-      
+      <MainComponent/>
+  
     </div>
   );
 }
